630b8f0502b021fff0eba9844855f6723ca44c6a
oktano <oktano@gmail.com>
2016-06-13 16:23:58 +0300

    Baseentity

--- crowdfunding-domain/src/main/java/com/reactiveiq/crowdfunding/domain/BaseEntity.java
+++ crowdfunding-domain/src/main/java/com/reactiveiq/crowdfunding/domain/BaseEntity.java
@@ -1,11 +1,13 @@
 package com.reactiveiq.crowdfunding.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Version;
 
+@Entity
 public class BaseEntity {
 	
 	private Long id;
